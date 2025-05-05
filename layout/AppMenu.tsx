549bc37e61cbb9003119f5ab81269928298a3449
SonMc <sonmc90@gmail.com>
2025-05-06 00:13:32 +0700

    init

---
+++ layout/AppMenu.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppMenuitem from './AppMenuitem';
+import { MenuProvider } from './context/menucontext';
+
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const model: AppMenuItem[] = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                }
+            ]
+        }
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
